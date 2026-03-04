1411fb4b106f4b389b68da6dbf5be07dc1eeff03
Artem Stadnik <stadnikartem1709@gmail.com>
2026-03-04 10:39:53 +0200

    fix: use ESM syntax in eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,13 @@
-
+export default {
+  extends: ['@mate-academy/eslint-config', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  env: { jest: true },
+  rules: {
+    'no-proto': 0,
+    'no-console': 'off',
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+  },
+  plugins: ['jest', '@typescript-eslint'],
+};
